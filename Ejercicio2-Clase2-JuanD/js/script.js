e63141bc7af3ce04c2061f99b6c7be3aa1a8cf9f
id4n <juand.sanchez@upb.edu.co>
2020-02-12 20:09:10 -0500

    html + css + js

---
+++ Ejercicio2-Clase2-JuanD/js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $("#boton").click(function () {
+        /*$("#1").css("display", "none");*/
+        var buscado = document.getElementById("miBusqueda").value;
+        window.alert(buscado);
+        /*var i;
+        for (i = 0; i < cars.length; i++) {
+            text += cars[i] + "<br>";
+        }*/
+    });
+});
